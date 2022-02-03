209a2e01f2ddf347cc0f4de598768553d091a566
Ben Botvinick <benbotvinick@gmail.com>
2022-02-02 21:45:50 -0800

    Modernize

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  purge: [
+  content: [
     './src/pages/**/*.js',
     './src/components/**/*.js',
-    './src/layouts/*.js',
   ],
   theme: {
     fontFamily: {
-      'sans': ['Verdana', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+      'sans': ['Helvetica'],
       'serif': ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
       'mono': ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
     }
